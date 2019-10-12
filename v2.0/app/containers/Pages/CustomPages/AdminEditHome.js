b274be2a21e5c7884386e79f6c3885af19dac5bf
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-10-12 16:39:09 -0600

    edit form looks better

--- v2.0/app/containers/Pages/CustomPages/AdminEditHome.js
+++ v2.0/app/containers/Pages/CustomPages/AdminEditHome.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
-import { styles, vanish, uploadBox } from './styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -18,7 +17,10 @@ import AboutUsDescription from './Frags/AboutUsDescription';
 import GraphChoice from './Frags/GraphChoice';
 import EventChoices from './Frags/EventChoices';
 import Public from '@material-ui/icons/Public';
-import { allCommunities, immediateEventQuest, immediateGraphQuest, formForJokes, iconTextDefaults } from './DataRetriever';
+import { styles, vanish, uploadBox } from './styles';
+import {
+ allCommunities, immediateEventQuest, immediateGraphQuest, formForJokes, iconTextDefaults 
+} from './DataRetriever';
 import VerificationModal from './Frags/VerificationModal';
 class AdminEditHome extends React.Component {
   constructor(props) {
@@ -32,19 +34,19 @@ class AdminEditHome extends React.Component {
     this.removeIconFeature = this.removeIconFeature.bind(this);
     this.publishContent = this.publishContent.bind(this);
     this.state = {
-      about_video_url: "",
-      name: "",
-      description: "",
+      about_video_url: '',
+      name: '',
+      description: '',
       selected_icon_features: [],
       selected_graphs: [],
       selected_events: [],
       communities: [],
       events: [],
       graphs: [],
-      selected_community: { id: null, name: "Choose Community" },
+      selected_community: { id: null, name: 'Choose Community' },
       files: [],
       summary_modal_is_open: false
-    }
+    };
   }
 
   componentDidMount() {
@@ -55,58 +57,64 @@ class AdminEditHome extends React.Component {
   handleVideoUrl = (event) => {
     this.setState({ about_video_url: event.target.value.trim() });
   }
+
   handleName = (event) => {
     this.setState({ name: event.target.value.trim() });
   }
+
   handleDescription = (event) => {
     this.setState({ description: event.target.value.trim() });
   }
+
   graphSearch = (id) => {
     const me = this;
     immediateGraphQuest(id)
       .then(res => {
-        me.setState({ graphs: res.data })
+        me.setState({ graphs: res.data });
       });
-
   }
+
   eventSearch = (id) => {
     const me = this;
     immediateEventQuest(id)
       .then(res => {
-        me.setState({ events: res.data })
+        me.setState({ events: res.data });
       });
-
   }
+
   callForAllCommunities = () => {
     const me = this;
     allCommunities().then(res => {
       me.setState({ communities: res.data });
     });
   }
+
   handleEventSelection = (item) => {
-    var old = this.state.selected_events;
+    let old = this.state.selected_events;
     if (old.length !== 3) {
       this.setState({ selected_events: old.includes(item) ? [...old] : [...old, item] });
     }
   }
+
   handleGraphSelection = (item) => {
-    var old = this.state.selected_graphs;
+    let old = this.state.selected_graphs;
     this.setState({ selected_graphs: old.includes(item) ? [...old] : [...old, item] });
   }
+
   addFeatures = (item) => {
-    var old = this.state.selected_icon_features;
+    let old = this.state.selected_icon_features;
     this.setState({ selected_icon_features: old.includes(item) ? [...old] : [...old, item] });
   }
 
   handleCommunitiesChoice = (event) => {
-    var obj = this.findCommunityObj(event.target.value);
+    let obj = this.findCommunityObj(event.target.value);
     this.setState({
       selected_community: obj,
       selected_events: [],
       events: [],
       graphs: [],
       selected_graphs: []
-    }); //also flash the event values just in case
+    }); // also flash the event values just in case
     this.eventSearch(obj.id);
     this.graphSearch(obj.id);
   }
@@ -116,80 +124,86 @@ class AdminEditHome extends React.Component {
     if (what === "value") {
       return arr[1];
     }
-    else if (what === "id") {
+    if (what === "id") {
       return arr[0];
     }
   }
 
   findCommunityObj = (name) => {
-    var section = this.state.communities;
-    for (var i = 0; i < section.length; i++) {
+    let section = this.state.communities;
+    for (let i = 0; i < section.length; i++) {
       if (section[i].name === name) {
         return section[i];
       }
     }
     return null;
   }
+
   showFileList() {
     const { files } = this.state;
-    if (files.length === 0) return "You have not selected any files. ";
-    var string = "";
-    for (var i = 0; i < files.length; i++) {
-      if (string !== "") {
-        string += ", " + files[i].name;
-      }
-      else {
+    if (files.length === 0) return 'You have not selected any files. ';
+    let string = '';
+    for (let i = 0; i < files.length; i++) {
+      if (string !== '') {
+        string += ', ' + files[i].name;
+      } else {
         string = files[i].name;
       }
     }
     return string;
   }
+
   removeIconFeature = (name) => {
-    var f = this.state.selected_icon_features.filter(itm => itm.name !== name);
+    let f = this.state.selected_icon_features.filter(itm => itm.name !== name);
     this.setState({ selected_icon_features: f });
   }
+
   removeGraph = (id) => {
-    var f = this.state.selected_graphs.filter(itm => itm.id !== id);
+    let f = this.state.selected_graphs.filter(itm => itm.id !== id);
     this.setState({ selected_graphs: f });
   }
+
   removeEvent = (id) => {
-    var f = this.state.selected_events.filter(itm => itm.id !== id);
+    let f = this.state.selected_events.filter(itm => itm.id !== id);
     this.setState({ selected_events: f });
   }
+
   trackSelectedFeatureEdit = (obj) => {
     const selected = this.state.selected_icon_features;
-    for (var i = 0; i < selected.length; i++) {
+    for (let i = 0; i < selected.length; i++) {
       if (selected[i].name === obj.name) {
         selected[i] = { ...obj };
       }
     }
     this.setState({ selected_icon_features: selected });
   }
+
   searchForIconDefaults = (name) => {
     switch (name) {
-      case "Events":
+      case 'Events':
         return iconTextDefaults.events;
-      case "Actions":
+      case 'Actions':
         return iconTextDefaults.actions;
-      case "Service Providers":
+      case 'Service Providers':
         return iconTextDefaults.service;
-      case "Testimonials":
+      case 'Testimonials':
         return iconTextDefaults.testimonials;
       default:
         break;
     }
   }
+
   cleanUpIconEdits = () => {
     const edits = this.state.selected_icon_features;
-    var icons = [];
-    for (var i = 0; i < edits.length; i++) {
-      var ico = edits[i];
-      var item = { ...ico };
-      var DEFAULT = this.searchForIconDefaults(ico.name);
-      if (ico.title.trim() === "") {
+    let icons = [];
+    for (let i = 0; i < edits.length; i++) {
+      let ico = edits[i];
+      let item = { ...ico };
+      let DEFAULT = this.searchForIconDefaults(ico.name);
+      if (ico.title.trim() === '') {
         item.title = DEFAULT.title;
       }
-      if (ico.desc.trim() === "") {
+      if (ico.desc.trim() === '') {
         item.desc = DEFAULT.desc;
       }
       item.name = ico.name;
@@ -197,15 +211,16 @@ class AdminEditHome extends React.Component {
     }
     return icons;
   }
+
   popIDs = (arr) => {
-    var idArray = [];
-    for (var i = 0; i < arr.length; i++) {
+    let idArray = [];
+    for (let i = 0; i < arr.length; i++) {
       idArray.push(arr[i].id);
     }
     return idArray;
   }
-  publishContent = () => {
 
+  publishContent = () => {
     const data = {
       chosen_community: this.state.selected_community.id,
       name: this.state.name,
@@ -216,15 +231,18 @@ class AdminEditHome extends React.Component {
       images: this.state.files,
       about_video_url: this.state.about_video_url
     };
-    console.log("I am the form data", data);
-    //formForJokes(data);
+    console.log('I am the form data', data);
+    // formForJokes(data);
   }
+
   openSummary = () => {
     this.setState({ summary_modal_is_open: true });
   }
+
   closeSummary = () => {
     this.setState({ summary_modal_is_open: false });
   }
+
   showSummary = () => {
     if (this.state.summary_modal_is_open) {
       return (
@@ -243,26 +261,29 @@ class AdminEditHome extends React.Component {
       );
     }
   }
+
   render() {
-    const communities = this.state.communities;
+    const {communities} = this.state;
     const { classes } = this.props;
     const community = this.state.selected_community.name;
-    //const { available_sections } = this.state;
-    //const { selected_sections } = this.state;
+    // const { available_sections } = this.state;
+    // const { selected_sections } = this.state;
     return (
       <div>
         {this.showSummary()}
-        <div style={{ margin: 30 }}></div>
+        <div style={{ margin: 30 }} />
         <Grid item xl={12} md={12}>
           <Paper className={classes.root} elevation={4}>
             <Fab
-              onClick={() => { this.openSummary() }}
+              onClick={() => { this.openSummary(); }}
               variant="extended"
               color="secondary"
               aria-label="Delete"
               className={classes.button}
             >
-              Finish Up <span style={{ margin: 3 }}></span>
+              Finish Up 
+              {' '}
+              <span style={{ margin: 3 }}></span>
               <Public />
             </Fab>
             <TextField
@@ -272,7 +293,7 @@ class AdminEditHome extends React.Component {
               className={classes.textField}
               value={community}
               fullWidth
-              onChange={option => { this.handleCommunitiesChoice(option) }}
+              onChange={option => { this.handleCommunitiesChoice(option); }}
               SelectProps={{
                 MenuProps: {
                   className: classes.menu,
@@ -290,7 +311,7 @@ class AdminEditHome extends React.Component {
             </TextField>
 
             <TextField
-              onChange={(event) => { this.handleName(event) }}
+              onChange={(event) => { this.handleName(event); }}
               fullWidth
               placeholder="Name"
               margin="normal"
@@ -298,7 +319,7 @@ class AdminEditHome extends React.Component {
               helperText="This will be the name of the homepage..."
             />
             <TextField
-              onChange={(event) => { this.handleDescription(event) }}
+              onChange={(event) => { this.handleDescription(event); }}
               id="outlined-multiline-flexible"
               label="Description"
               fullWidth
@@ -314,13 +335,17 @@ class AdminEditHome extends React.Component {
             />
             <div style={uploadBox}>
               <Typography className={Type.textGrey} gutterBottom>
-                Upload exactly <b> ( 3 ) </b> images
-              </Typography>
+                Upload exactly 
+            {' '}
+            <b> ( 3 ) </b>
+            {' '}
+            images
+            </Typography>
               <Typography className={Type.textGreyLight} gutterBottom>
                 {this.showFileList()}
               </Typography>
               <input
-                onChange={info => { this.setState({ files: info.target.files }) }}
+                onChange={info => { this.setState({ files: info.target.files }); }}
                 style={vanish}
                 accept="image/*"
                 className={classes.inputUpload}
@@ -364,19 +389,21 @@ class AdminEditHome extends React.Component {
             removeFeatureFxn={this.removeIconFeature}
           />
           <Fab
-            onClick={() => { this.openSummary(); window.scrollTo(0, 20) }}
+            onClick={() => { this.openSummary(); window.scrollTo(0, 20); }}
             variant="extended"
             color="secondary"
             aria-label="Delete"
             style={{ margin: 20, float: 'right' }}
             className={classes.button}
           >
-            Finish Up <span style={{ margin: 3 }}></span>
+            Finish Up 
+{' '}
+<span style={{ margin: 3 }}></span>
             <Public />
           </Fab>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
