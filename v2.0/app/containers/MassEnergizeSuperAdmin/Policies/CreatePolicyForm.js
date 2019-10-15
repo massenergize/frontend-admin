011ba56802b2fec5ef77f78041af1304c16f8502
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-10-15 07:39:16 -0600

    policy creation working

--- v2.0/app/containers/MassEnergizeSuperAdmin/Policies/CreatePolicyForm.js
+++ v2.0/app/containers/MassEnergizeSuperAdmin/Policies/CreatePolicyForm.js
@@ -2,35 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import { Field, reduxForm } from 'redux-form/immutable';
-import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+
+import { reduxForm } from 'redux-form/immutable';
+import { bindActionCreators } from 'redux';
+import FormControl from '@material-ui/core/FormControl';
+import Select2 from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import Grid from '@material-ui/core/Grid';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Typography from '@material-ui/core/Typography';
-import { bindActionCreators } from 'redux';
+import TextField from '@material-ui/core/TextField';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
-import {
-  Checkbox,
-  Select,
-  TextField,
-  Switch
-} from 'redux-form-material-ui';
+import { apiCall } from '../../../utils/messenger';
+import MySnackbarContentWrapper from '../../../components/SnackBar/SnackbarContentWrapper';
+
 import { initAction, clearAction } from '../../../actions/ReduxFormActions';
 
-const renderRadioGroup = ({ input, ...rest }) => (
-  <RadioGroup
-    {...input}
-    {...rest}
-    valueselected={input.value}
-    onChange={(event, value) => input.onChange(value)}
-  />
-);
 
 // validation functions
 const required = value => (value == null ? 'Required' : undefined);
@@ -64,74 +51,197 @@ const styles = theme => ({
   },
 });
 
-const initData = {
-  text: 'Sample Text',
-  email: 'sample@mail.com',
-  radio: 'option1',
-  selection: 'option1',
-  onof: true,
-  checkbox: true,
-  textarea: 'This is default text'
-};
 
 class CreatePolicyForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formData: {},
+      communities: [],
+      teams: [],
+      is_community_goal: true,
+      error: null,
+      successMsg: null
+    };
+  }
+
+
+  async componentDidMount() {
+    const communitiesResponse = await apiCall('/communities.listForSuperAdmin');
+    if (communitiesResponse && communitiesResponse.success) {
+      this.setStateAsync({ communities: communitiesResponse.data });
+    }
+  }
+
+  setStateAsync(state) {
+    return new Promise((resolve) => {
+      this.setState(state, resolve);
+    });
+  }
+
+  handleFormDataChange = (event) => {
+    const { target } = event;
+    if (!target) return;
+    const { name, value } = target;
+    const { formData } = this.state;
+    this.setState({
+      formData: { ...formData, [name]: value }
+    });
+  }
+
+  handleCloseStyle = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    this.setState({ successMsg: null, error: null });
+  };
+
+  submitForm = async (event) => {
+    event.preventDefault();
+    const { formData } = this.state;
+    const cleanedValues = { ...formData };
+    if (cleanedValues.community) {
+      cleanedValues.community_id = cleanedValues.community;
+    }
+    if (cleanedValues.team) {
+      cleanedValues.team_id = cleanedValues.team;
+    }
+    delete cleanedValues.community;
+    delete cleanedValues.team;
+
+    const response = await apiCall('/policies.create', cleanedValues);
+    if (response && !response.success) {
+      await this.setStateAsync({ error: response.error, successMsg: null });
+    } else if (response && response.success) {
+      await this.setStateAsync({ successMsg: 'Successfully Created this Policy', error: null });
+      window.location.href = '/admin/read/policies';
+    }
+  }
+
+  async updateForm(fieldName, value) {
+    const { formData } = this.state;
+    await this.setStateAsync({
+      formData: {
+        ...formData,
+        [fieldName]: value
+      }
+    }
+    );
+  }
+
+
+
   render() {
     const trueBool = true;
     const {
       classes,
-      handleSubmit,
-      pristine,
-      reset,
       submitting,
-      init,
-      clear
     } = this.props;
+    const {
+      formData, communities, error, successMsg
+    } = this.state;
+    const { community, team } = formData;
+    let communitySelected = communities.filter(c => c.id === community)[0];
+    communitySelected = communitySelected ? communitySelected.name : '';
+
+
     return (
       <div>
         <Grid container spacing={24} alignItems="flex-start" direction="row" justify="center">
           <Grid item xs={12} md={6}>
             <Paper className={classes.root}>
-              
-              <form onSubmit={handleSubmit}>
-                <div>
-                  <Field
-                    name="title"
-                    component={TextField}
-                    placeholder="eg. Terms and Conditions"
-                    label="Policy Title"
-                    validate={required}
-                    required
-                    ref={this.saveRef}
-                    className={classes.field}
+              <form onSubmit={this.submitForm}>
+
+                <Snackbar
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  open={error != null}
+                  autoHideDuration={6000}
+                  onClose={this.handleCloseStyle}
+                >
+                  <MySnackbarContentWrapper
+                    onClose={this.handleCloseStyle}
+                    variant="error"
+                    message={`Error Occurred: ${error}`}
                   />
-                </div>
-      
-                <div>
-                  <FormControl className={classes.field}>
-                    <InputLabel htmlFor="selection">Community</InputLabel>
-                    <Field
-                      name="selection"
-                      component={Select}
-                      placeholder="Selection"
-                      autoWidth={trueBool}
-                    >
-                      <MenuItem value="option1">Wayland</MenuItem>
-                      <MenuItem value="option2">Concord</MenuItem>
-                      <MenuItem value="option3">Global</MenuItem>
-                    </Field>
-                  </FormControl>
-                </div>
-                <div className={classes.field}>
-                  <Field
-                    name="description"
-                    className={classes.field}
-                    component={TextField}
-                    placeholder="eg. This policy is about ..."
-                    label="Description"
-                    multiline={trueBool}
-                    rows={4}
+                </Snackbar>
+                <Snackbar
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  open={successMsg != null}
+                  autoHideDuration={6000}
+                  onClose={this.handleCloseStyle}
+                >
+                  <MySnackbarContentWrapper
+                    onClose={this.handleCloseStyle}
+                    variant="success"
+                    message={successMsg}
                   />
-                </div>
+                </Snackbar>
+
+
+                {error
+                && (
+                  <p style={{ color: 'red' }}>{error}</p>
+                )
+                }
+                {successMsg
+                && (
+                  <p style={{ color: 'green' }}>{successMsg}</p>
+                )
+                }
+
+
+                <TextField
+                  id="outline-required"
+                  required
+                  name="name"
+                  onChange={this.handleFormDataChange}
+                  label="Name"
+                  placeholder="eg. Take 10,000 actions"
+                  className={classes.field}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                
+
+                <FormControl className={classes.field}>
+                  <InputLabel htmlFor="community_id">Community</InputLabel>
+                  <Select2
+                    native
+                    name="community_id"
+                    onChange={async (newValue) => { await this.updateForm('community_id', parseInt(newValue.target.value, 10)); }}
+                    inputProps={{
+                      id: 'age-native-simple',
+                    }}
+                  >
+                    <option value={community}>{communitySelected}</option>
+                    { communities
+                          && communities.map(c => (
+                            <option value={c.id} key={c.id}>{c.name}</option>
+                          ))
+                    }
+                  </Select2>
+                </FormControl>
+
+                <TextField
+                  name="description"
+                  className={classes.field}
+                  placeholder="eg. Description ..."
+                  label="Describe this Goal"
+                  multiline={trueBool}
+                  rows={4}
+                  onChange={this.handleFormDataChange}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+
                 <div>
                   <Button variant="contained" color="secondary" type="submit" disabled={submitting}>
                     Submit
@@ -146,9 +256,6 @@ class CreatePolicyForm extends Component {
   }
 }
 
-renderRadioGroup.propTypes = {
-  input: PropTypes.object.isRequired,
-};
 
 CreatePolicyForm.propTypes = {
   classes: PropTypes.object.isRequired,
