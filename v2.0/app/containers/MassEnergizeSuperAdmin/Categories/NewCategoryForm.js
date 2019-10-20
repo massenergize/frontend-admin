50edd6f5c7e3c16b12322cfb5ddf87635e032df2
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-10-19 23:55:54 -0600

    updates to policy

--- v2.0/app/containers/MassEnergizeSuperAdmin/Categories/NewCategoryForm.js
+++ v2.0/app/containers/MassEnergizeSuperAdmin/Categories/NewCategoryForm.js
@@ -1,51 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import { Field, reduxForm } from 'redux-form/immutable';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
-import Grid from '@material-ui/core/Grid';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Typography from '@material-ui/core/Typography';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import Button from '@material-ui/core/Button';
-import Type from 'dan-styles/Typography.scss';
-import Fab from '@material-ui/core/Fab';
-import {
-  Checkbox,
-  Select,
-  TextField,
-  Switch
-} from 'redux-form-material-ui';
-import { initAction, clearAction } from '../../../actions/ReduxFormActions';
+import { apiCall } from '../../../utils/messenger';
+import MassEnergizeForm from '../_FormGenerator';
 
-const renderRadioGroup = ({ input, ...rest }) => (
-  <RadioGroup
-    {...input}
-    {...rest}
-    valueselected={input.value}
-    onChange={(event, value) => input.onChange(value)}
-  />
-);
-
-// validation functions
-const required = value => (value == null ? 'Required' : undefined);
-const email = value => (
-  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-    ? 'Invalid email'
-    : undefined
-);
-const uploadBox = {
-  border: 'solid 1px #e0e0e0',
-  borderRadius: 5,
-  padding: 25
-}
 const styles = theme => ({
   root: {
     flexGrow: 1,
@@ -70,127 +28,87 @@ const styles = theme => ({
   },
 });
 
-const initData = {
-  text: 'Sample Text',
-  email: 'sample@mail.com',
-  radio: 'option1',
-  selection: 'option1',
-  onof: true,
-  checkbox: true,
-  textarea: 'This is default text'
-};
 
-class NewCategoryForm extends Component {
+class CreateNewTagCollectionForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      files: []
-    }
-  }
-  showFileList() {
-    const { files } = this.state;
-    if (files.length === 0) return "You have not selected any image ";
-    var string = "";
-    for (var i = 0; i < files.length; i++) {
-      if (string !== "") {
-        string += ", " + files[i].name;
-      }
-      else {
-        string = files[i].name;
-      }
-    }
-    return string;
+      formJson: null
+    };
   }
 
-  handleTexts = (event)=>{
-    this.setState({ [event.target.name]:event.target.value});
+
+  async componentDidMount() {
+    const formJson = await this.createFormJson();
+    await this.setStateAsync({ formJson });
   }
+
+  setStateAsync(state) {
+    return new Promise((resolve) => {
+      this.setState(state, resolve);
+    });
+  }
+
+  createFormJson = async () => {
+    const formJson = {
+      title: 'Create New Team',
+      subTitle: '',
+      method: '/tag_collections.create',
+      successRedirectPage: '/admin/read/categories',
+      fields: [
+        {
+          label: 'About this Tag Collection',
+          fieldType: 'Section',
+          children: [
+            {
+              name: 'name',
+              label: 'Name of Tag Collection',
+              placeholder: 'eg. Category',
+              fieldType: 'TextField',
+              contentType: 'text',
+              isRequired: true,
+              defaultValue: '',
+              dbName: 'name',
+              readOnly: false
+            },
+            {
+              name: 'tags',
+              label: 'Please write the tags separated by commas',
+              placeholder: 'eg. Low, Medium, High',
+              fieldType: 'TextField',
+              contentType: 'text',
+              isRequired: true,
+              isMultiline: true,
+              defaultValue: '',
+              dbName: 'description',
+              readOnly: false
+            },
+          ]
+        },
+      ]
+    };
+    return formJson;
+  }
+
+
   render() {
-    const trueBool = true;
-    const {
-      classes,
-      handleSubmit,
-      pristine,
-      reset,
-      submitting,
-      init,
-      clear
-    } = this.props;
+    const { classes } = this.props;
+    const { formJson } = this.state;
+    if (!formJson) return (<div />);
     return (
       <div>
-        <Grid container spacing={24} alignItems="flex-start" direction="row" justify="center">
-          <Grid item xs={12} md={10} sm={12} xs={12}>
-            <Paper className={classes.root}>
-              <h4 style={{color:'#585858',fontWeight:"500"}}>Use this form to create a new tag/category</h4>
-              <TextField
-              name = "title"
-                onChange={(event) => { this.handleTexts(event) }}
-                fullWidth
-                placeholder="Title"
-                margin="normal"
-                variant="outlined"
-                helperText="Add the title of this category"
-              />
-              <TextField
-              name = "description"
-                onChange={(event) => {this.handleTexts(event) }}
-                id="outlined-multiline-flexible"
-                label="Description"
-                fullWidth
-                multiline
-                cols="20"
-                rowsMax="19"
-                rows="10"
-                placeholder="Write a description for this category..."
-                className={classes.textField}
-                margin="normal"
-                helperText="Describe the category..."
-                variant="outlined"
-              />
-              <div style={uploadBox}>
-                <Typography className={Type.textGrey} gutterBottom>
-                  Upload an image
-              </Typography>
-                <Typography className={Type.textGreyLight} gutterBottom>
-                  {this.showFileList()}
-              </Typography>
-                <input
-                  onChange={info => { this.setState({ files: info.target.files }) }}
-                  style={{ display: 'none' }}
-                  accept="image/*"
-                  className={classes.inputUpload}
-                  id="raised-button-file"
-                  type="file"
-                />
-                { /* eslint-disable-next-line */}
-                <label htmlFor="raised-button-file">
-                  <Button
-                    variant="contained"
-                    component="span"
-                    id="raised-button-file"
-                    className={classes.button}
-                  >
-                    Upload
-                </Button>
-                </label>
-              </div>
-              <Fab
-                justify="right"
-                style={{ margin: 6, background: 'green' }}
-                onClick={() => { console.log( {title:"",description:"",...this.state}) }}
-                variant="extended"
-                color="secondary"
-                aria-label="Delete"
-                className={classes.button}
-              > Add Category </Fab>
-            </Paper>
-          </Grid>
-        </Grid>
+        <MassEnergizeForm
+          classes={classes}
+          formJson={formJson}
+        />
       </div>
     );
   }
 }
 
+CreateNewTagCollectionForm.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 
-export default withStyles(styles)(NewCategoryForm);
+export default withStyles(styles, { withTheme: true })(CreateNewTagCollectionForm);
--- v2.0/app/containers/Pages/CustomPages/tmp.js
+++ v2.0/app/containers/Pages/CustomPages/tmp.js
@@ -1230,3 +1230,491 @@ const FormInit = connect(
 )(ReduxFormMapped);
 
 export default withStyles(styles, { withTheme: true })(FormInit);
+
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import { Field, reduxForm } from 'redux-form/immutable';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import Grid from '@material-ui/core/Grid';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Typography from '@material-ui/core/Typography';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import Type from 'dan-styles/Typography.scss';
+import Fab from '@material-ui/core/Fab';
+import {
+  Checkbox,
+  Select,
+  TextField,
+  Switch
+} from 'redux-form-material-ui';
+import { initAction, clearAction } from '../../../actions/ReduxFormActions';
+
+const renderRadioGroup = ({ input, ...rest }) => (
+  <RadioGroup
+    {...input}
+    {...rest}
+    valueselected={input.value}
+    onChange={(event, value) => input.onChange(value)}
+  />
+);
+
+// validation functions
+const required = value => (value == null ? 'Required' : undefined);
+const email = value => (
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? 'Invalid email'
+    : undefined
+);
+const uploadBox = {
+  border: 'solid 1px #e0e0e0',
+  borderRadius: 5,
+  padding: 25
+}
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    padding: 30
+  },
+  field: {
+    width: '100%',
+    marginBottom: 20
+  },
+  fieldBasic: {
+    width: '100%',
+    marginBottom: 20,
+    marginTop: 10
+  },
+  inlineWrap: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  buttonInit: {
+    margin: theme.spacing.unit * 4,
+    textAlign: 'center'
+  },
+});
+
+const initData = {
+  text: 'Sample Text',
+  email: 'sample@mail.com',
+  radio: 'option1',
+  selection: 'option1',
+  onof: true,
+  checkbox: true,
+  textarea: 'This is default text'
+};
+
+class NewCategoryForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: []
+    }
+  }
+  showFileList() {
+    const { files } = this.state;
+    if (files.length === 0) return "You have not selected any image ";
+    var string = "";
+    for (var i = 0; i < files.length; i++) {
+      if (string !== "") {
+        string += ", " + files[i].name;
+      }
+      else {
+        string = files[i].name;
+      }
+    }
+    return string;
+  }
+
+  handleTexts = (event)=>{
+    this.setState({ [event.target.name]:event.target.value});
+  }
+  render() {
+    const trueBool = true;
+    const {
+      classes,
+      handleSubmit,
+      pristine,
+      reset,
+      submitting,
+      init,
+      clear
+    } = this.props;
+    return (
+      <div>
+        <Grid container spacing={24} alignItems="flex-start" direction="row" justify="center">
+          <Grid item xs={12} md={10} sm={12} xs={12}>
+            <Paper className={classes.root}>
+              <h4 style={{color:'#585858',fontWeight:"500"}}>Use this form to create a new tag/category</h4>
+              <TextField
+              name = "title"
+                onChange={(event) => { this.handleTexts(event) }}
+                fullWidth
+                placeholder="Title"
+                margin="normal"
+                variant="outlined"
+                helperText="Add the title of this category"
+              />
+              <TextField
+              name = "description"
+                onChange={(event) => {this.handleTexts(event) }}
+                id="outlined-multiline-flexible"
+                label="Description"
+                fullWidth
+                multiline
+                cols="20"
+                rowsMax="19"
+                rows="10"
+                placeholder="Write a description for this category..."
+                className={classes.textField}
+                margin="normal"
+                helperText="Describe the category..."
+                variant="outlined"
+              />
+              <div style={uploadBox}>
+                <Typography className={Type.textGrey} gutterBottom>
+                  Upload an image
+              </Typography>
+                <Typography className={Type.textGreyLight} gutterBottom>
+                  {this.showFileList()}
+              </Typography>
+                <input
+                  onChange={info => { this.setState({ files: info.target.files }) }}
+                  style={{ display: 'none' }}
+                  accept="image/*"
+                  className={classes.inputUpload}
+                  id="raised-button-file"
+                  type="file"
+                />
+                { /* eslint-disable-next-line */}
+                <label htmlFor="raised-button-file">
+                  <Button
+                    variant="contained"
+                    component="span"
+                    id="raised-button-file"
+                    className={classes.button}
+                  >
+                    Upload
+                </Button>
+                </label>
+              </div>
+              <Fab
+                justify="right"
+                style={{ margin: 6, background: 'green' }}
+                onClick={() => { console.log( {title:"",description:"",...this.state}) }}
+                variant="extended"
+                color="secondary"
+                aria-label="Delete"
+                className={classes.button}
+              > Add Category </Fab>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+
+
+export default withStyles(styles)(NewCategoryForm);
+
+
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Snackbar from '@material-ui/core/Snackbar';
+
+import { reduxForm } from 'redux-form/immutable';
+import { bindActionCreators } from 'redux';
+import FormControl from '@material-ui/core/FormControl';
+import Select2 from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import { apiCall } from '../../../utils/messenger';
+import MySnackbarContentWrapper from '../../../components/SnackBar/SnackbarContentWrapper';
+
+import { initAction, clearAction } from '../../../actions/ReduxFormActions';
+
+
+// validation functions
+const required = value => (value == null ? 'Required' : undefined);
+const email = value => (
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? 'Invalid email'
+    : undefined
+);
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    padding: 30
+  },
+  field: {
+    width: '100%',
+    marginBottom: 20
+  },
+  fieldBasic: {
+    width: '100%',
+    marginBottom: 20,
+    marginTop: 10
+  },
+  inlineWrap: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  buttonInit: {
+    margin: theme.spacing.unit * 4,
+    textAlign: 'center'
+  },
+});
+
+
+class CreatePolicyForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formData: {},
+      communities: [],
+      error: null,
+      successMsg: null
+    };
+  }
+
+
+  async componentDidMount() {
+    const communitiesResponse = await apiCall('/communities.listForSuperAdmin');
+    if (communitiesResponse && communitiesResponse.success) {
+      this.setStateAsync({ communities: communitiesResponse.data });
+    }
+  }
+
+  setStateAsync(state) {
+    return new Promise((resolve) => {
+      this.setState(state, resolve);
+    });
+  }
+
+  handleFormDataChange = (event) => {
+    const { target } = event;
+    if (!target) return;
+    const { name, value } = target;
+    const { formData } = this.state;
+    this.setState({
+      formData: { ...formData, [name]: value }
+    });
+  }
+
+  handleCloseStyle = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    this.setState({ successMsg: null, error: null });
+  };
+
+  submitForm = async (event) => {
+    event.preventDefault();
+    const { formData } = this.state;
+    const cleanedValues = { ...formData };
+    if (cleanedValues.community) {
+      cleanedValues.community_id = cleanedValues.community;
+    }
+    if (cleanedValues.team) {
+      cleanedValues.team_id = cleanedValues.team;
+    }
+    delete cleanedValues.community;
+    delete cleanedValues.team;
+
+    const response = await apiCall('/policies.create', cleanedValues);
+    if (response && !response.success) {
+      await this.setStateAsync({ error: response.error, successMsg: null });
+    } else if (response && response.success) {
+      await this.setStateAsync({ successMsg: 'Successfully Created this Policy', error: null });
+      window.location.href = '/admin/read/policies';
+    }
+  }
+
+  async updateForm(fieldName, value) {
+    const { formData } = this.state;
+    await this.setStateAsync({
+      formData: {
+        ...formData,
+        [fieldName]: value
+      }
+    }
+    );
+  }
+
+
+
+  render() {
+    const trueBool = true;
+    const {
+      classes,
+      submitting,
+    } = this.props;
+    const {
+      formData, communities, error, successMsg
+    } = this.state;
+    const { community } = formData;
+    let communitySelected = communities.filter(c => c.id === community)[0];
+    communitySelected = communitySelected ? communitySelected.name : '';
+
+
+    return (
+      <div>
+        <Grid container spacing={24} alignItems="flex-start" direction="row" justify="center">
+          <Grid item xs={12} md={6}>
+            <Paper className={classes.root}>
+              <form onSubmit={this.submitForm}>
+
+                <Snackbar
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  open={error != null}
+                  autoHideDuration={6000}
+                  onClose={this.handleCloseStyle}
+                >
+                  <MySnackbarContentWrapper
+                    onClose={this.handleCloseStyle}
+                    variant="error"
+                    message={`Error Occurred: ${error}`}
+                  />
+                </Snackbar>
+                <Snackbar
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  open={successMsg != null}
+                  autoHideDuration={6000}
+                  onClose={this.handleCloseStyle}
+                >
+                  <MySnackbarContentWrapper
+                    onClose={this.handleCloseStyle}
+                    variant="success"
+                    message={successMsg}
+                  />
+                </Snackbar>
+
+
+                {error
+                && (
+                  <p style={{ color: 'red' }}>{error}</p>
+                )
+                }
+                {successMsg
+                && (
+                  <p style={{ color: 'green' }}>{successMsg}</p>
+                )
+                }
+
+
+                <TextField
+                  id="outline-required"
+                  required
+                  name="name"
+                  onChange={this.handleFormDataChange}
+                  label="Name"
+                  placeholder="eg. Terms and Conditions"
+                  className={classes.field}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                
+
+                <FormControl className={classes.field}>
+                  <InputLabel htmlFor="community_id">Community</InputLabel>
+                  <Select2
+                    native
+                    name="community_id"
+                    onChange={async (newValue) => { await this.updateForm('community_id', parseInt(newValue.target.value, 10)); }}
+                    inputProps={{
+                      id: 'age-native-simple',
+                    }}
+                  >
+                    <option value={community}>{communitySelected}</option>
+                    { communities
+                          && communities.map(c => (
+                            <option value={c.id} key={c.id}>{c.name}</option>
+                          ))
+                    }
+                  </Select2>
+                </FormControl>
+
+                <TextField
+                  name="description"
+                  className={classes.field}
+                  placeholder="eg. Our terms and conditions ..."
+                  label="Describe this Goal"
+                  multiline={trueBool}
+                  rows={4}
+                  onChange={this.handleFormDataChange}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+
+                <div>
+                  <Button variant="contained" color="secondary" type="submit" disabled={submitting}>
+                    Submit
+                  </Button>
+                </div>
+              </form>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+
+CreatePolicyForm.propTypes = {
+  classes: PropTypes.object.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  reset: PropTypes.func.isRequired,
+  pristine: PropTypes.bool.isRequired,
+  submitting: PropTypes.bool.isRequired,
+  init: PropTypes.func.isRequired,
+  clear: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+  init: bindActionCreators(initAction, dispatch),
+  clear: () => dispatch(clearAction),
+});
+
+const ReduxFormMapped = reduxForm({
+  form: 'immutableExample',
+  enableReinitialize: true,
+})(CreatePolicyForm);
+
+const reducer = 'initval';
+const FormInit = connect(
+  state => ({
+    force: state,
+    initialValues: state.getIn([reducer, 'formValues'])
+  }),
+  mapDispatchToProps,
+)(ReduxFormMapped);
+
+export default withStyles(styles)(FormInit);
