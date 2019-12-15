6cac822d66eb46cd659fe75da60615fa455b4661
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-12-15 05:02:23 -0700

    community profile files export

--- v2.0/app/containers/MassEnergizeSuperAdmin/Community/Profile/index.js
+++ v2.0/app/containers/MassEnergizeSuperAdmin/Community/Profile/index.js
@@ -1,10 +1,17 @@
-import CommunityData from "./CommunityData";
+import About from './About';
+import Albums from './Albums';
+import Connection from './Connection';
+import Favorites from './Favorites';
+import Cover from './Cover';
+import Pages from './Pages';
+import CommunityData from './CommunityData';
 
-
-export About from './About';
-export Albums from './Albums';
-export Connection from './Connection';
-export Favorites from './Favorites';
-export Cover from './Cover';
-export Pages from './Pages';
-export CommunityData from './CommunityData';
+module.exports = {
+  About,
+  Albums,
+  Connection,
+  Favorites,
+  Cover,
+  Pages,
+  CommunityData,
+};
