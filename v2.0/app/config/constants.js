f51e31eea90d6ad13d030fd07b23b34d455fe900
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2020-04-21 21:36:04 -0600

    updates

--- v2.0/app/config/constants.js
+++ v2.0/app/config/constants.js
@@ -4,7 +4,7 @@
 
 const APP_NAME = 'MassEnergize Administration';
 const BUILD_VERSION = '0.9.9.9';
-const IS_LOCAL = false;
+const IS_LOCAL = true;
 const IS_PROD = false;
 
 //  ---- setting  API routes
