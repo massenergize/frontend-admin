06f503670ebe911650d125e49547da0f8225eb01
Brad Hubbard-Nelson <bradhn@mindspring.com>
2021-10-23 19:39:48 -0400

    Issue 344 - can't delete a parent team

--- app/containers/MassEnergizeSuperAdmin/Teams/EditTeam.js
+++ app/containers/MassEnergizeSuperAdmin/Teams/EditTeam.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import { apiCall } from '../../../utils/messenger';
-import MassEnergizeForm from '../_FormGenerator';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { apiCall } from '../../../utils/messenger';
+import MassEnergizeForm from '../_FormGenerator';
 import { reduxGetAllCommunityTeams } from '../../../redux/redux-actions/adminActions';
 
 const styles = theme => ({
@@ -40,7 +40,6 @@ class EditTeam extends Component {
     this.state = {
       communities: [],
       formJson: null,
-      addTeamAdminJson: null,
       team: null,
       parentTeamOptions: false,
     };
@@ -49,25 +48,23 @@ class EditTeam extends Component {
 
   async componentDidMount() {
     const { id } = this.props.match.params;
-    let parentTeamOptions = "0";    
+    let parentTeamOptions = '0';
     const teamResponse = await apiCall('/teams.info', { team_id: id });
     if (teamResponse && teamResponse.data) {
       const team = teamResponse.data;
-
       if (team.primary_community) {
         // const teams = await this.props.callTeamsForNormalAdmin();
         const teams = await apiCall('/teams.list', { community_id: team.primary_community.id });
+
         // if other teams have us as a parent, can't set a parent ourselves
         // from that point, can set parent teams that are not ourselves AND don't have parents themselves (i.e. aren't sub-teams)
         const parentTeams = teams.data.filter(_team => _team.parent && _team.parent.id === team.id).length === 0
           && teams.data.filter(_team => ((_team.id !== team.id) && !_team.parent));
         if (parentTeams) {
           parentTeamOptions = parentTeams.map(_team => ({ id: _team.id, displayName: _team.name }));
-          parentTeamOptions = [{displayName:"NONE", id:"0"}, ...parentTeamOptions]
-        }
-        else
-        {
-          parentTeamOptions = [{displayName:"Team cannot have a parent, because it is a parent of another team", id:"0"}];
+          parentTeamOptions = [{ displayName: 'NONE', id: '0' }, ...parentTeamOptions];
+        } else {
+          parentTeamOptions = [{ displayName: 'Team cannot have a parent, because it is a parent of another team', id: '0' }];
         }
       }
       await this.setStateAsync({ team, parentTeamOptions });
@@ -92,6 +89,7 @@ class EditTeam extends Component {
   createFormJson = async () => {
     const { communities, team, parentTeamOptions } = this.state;
     const selectedCommunities = team.communities ? team.communities.map(e => '' + e.id) : [];
+
     const formJson = {
       title: 'Edit Team Information',
       subTitle: '',
@@ -129,9 +127,10 @@ class EditTeam extends Component {
               label: 'Primary Community',
               placeholder: '',
               fieldType: 'Dropdown',
-              defaultValue: team.primary_community && team.primary_community.id,
+              defaultValue: (team.primary_community && team.primary_community.id) || '0',
               dbName: 'primary_community_id',
-              data: [{displayName:"--", id:""}, ...communities],
+              data: [{ displayName: '--', id: '0' }, ...communities],
+              readOnly: false,
             },
             {
               name: 'communities',
@@ -143,14 +142,14 @@ class EditTeam extends Component {
               dbName: 'communities',
               data: communities,
             },
-             {
+            {
               name: 'parent',
               label: parentTeamOptions && 'Choose a Parent Team, if this team is part of a larger group (not the usual case)',
               fieldType: 'Dropdown',
-              defaultValue: (team.parent && team.parent.id) || "0",
+              defaultValue: (team.parent && team.parent.id) || '0',
               dbName: 'parent_id',
               data: parentTeamOptions,
-              readOnly: parentTeamOptions && false || true,
+              readOnly: (parentTeamOptions && false) || true,
             },
             {
               name: 'tagline',
