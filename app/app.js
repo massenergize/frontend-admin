bd034fc5f7a83c98a4a9bf0430dc7ae5073e1425
Tahiru <abdullai.tahiru@gmail.com>
2022-06-16 20:37:30 +0000

    pushed secrete keys to .env file

--- app/app.js
+++ app/app.js
@@ -41,10 +41,7 @@ import { IS_CANARY, IS_PROD } from './config/constants';
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
-const SENTRY_DSN =
-  IS_PROD || IS_CANARY
-    ? "https://24efee7f2801430c99913cd39a99259b@o415460.ingest.sentry.io/5306644"
-    : "https://f2e6d5eedbc744ff821b6a98f73914ef@o415460.ingest.sentry.io/5306645";
+const SENTRY_DSN = IS_PROD || IS_CANARY? process.env.REACT_APP_SENTRY_PROD_DSN: process.env.REACT_APP_SENTRY_DEV_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
