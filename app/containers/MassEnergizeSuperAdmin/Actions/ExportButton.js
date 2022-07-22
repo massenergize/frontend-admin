2a93f5fadb77614cb5912e7199ece230f22d9985
Mattia Danese <mattiadanese@Mattias-MacBook-Air.local>
2022-07-22 15:05:50 -0400

    doc api env var & drive icon

--- app/containers/MassEnergizeSuperAdmin/Actions/ExportButton.js
+++ app/containers/MassEnergizeSuperAdmin/Actions/ExportButton.js
@@ -5,11 +5,12 @@ import FileCopy from "@material-ui/icons/FileCopy";
 export default function ExportButton(props) {
     const [openPicker, data, authResponse] = useDrivePicker();
     const action_id = props.id;
+    const DOC_API_KEY = process.env.REACT_APP_DOC_API_KEY
 
     const handleOnClick = (action_id) => {
         openPicker({
             clientId: "941856327176-691hf1ndva0423cch73k9ok0a6do0gs8.apps.googleusercontent.com", // web client credentials
-            developerKey: DOCS_DEV_KEY, // Import/Export Docs API Key
+            developerKey: DOC_API_KEY, // Import/Export Docs API Key
             viewId: "FOLDERS",
             supportDrives: true,
             supportsAllDrives: true,
@@ -24,6 +25,6 @@ export default function ExportButton(props) {
         })
     }
 
-    return <i className="fab fa-google material-ui" onClick={() => handleOnClick(action_id)} /> 
+    return <i className="fab fa-google-drive" onClick={() => handleOnClick(action_id)} /> 
     // return <FileCopy color="secondary" onClick={() => handleOnClick(action_id)}>Ion-logo-google</FileCopy>
 }
