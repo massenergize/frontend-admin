8891e9b0874d16707c4ca01a44a6e018a9804d53
Mattia Danese <mattiadanese@Mattias-MacBook-Air.local>
2022-08-08 14:39:00 -0400

    regex onChange function checks subdomain input

--- app/containers/MassEnergizeSuperAdmin/_FormGenerator/index.js
+++ app/containers/MassEnergizeSuperAdmin/_FormGenerator/index.js
@@ -221,6 +221,24 @@ class MassEnergizeForm extends Component {
     });
   };
 
+  handleSubDomainChange = async (event) => {
+    const { target } = event;
+    if (!target) return;
+    
+    const { formData } = this.state;
+    const { name, value } = target;
+    
+    if (!value) return;
+
+    // does not leave international characters like ä
+    const newValue = value.replaceAll(' ', '_').replaceAll(/[^a-zA-Z0-9_]/g, '');
+
+    event.target.value = newValue;
+    await this.setStateAsync({
+        formData: { ...formData, [name]: newValue},
+    });
+  }
+
   /**
    * Handle checkboxes when they are clicked
    */
@@ -911,7 +929,7 @@ class MassEnergizeForm extends Component {
             <TextField
               required={field.isRequired}
               name={field.name}
-              onChange={this.handleFormDataChange}
+              onChange={field.name === "subdomain" ? this.handleSubDomainChange : this.handleFormDataChange}
               label={field.label}
               multiline={field.isMultiline}
               rows={4}
