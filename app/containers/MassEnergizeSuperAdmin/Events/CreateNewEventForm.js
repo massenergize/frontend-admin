8fdeb0d966235be13ac467b313b1acf9328a146c
frimpongopoku <pongofrimi@Gmail.com>
2022-08-18 10:47:41 +0400

    end date autofills, and dates are properly validated

--- app/containers/MassEnergizeSuperAdmin/Events/CreateNewEventForm.js
+++ app/containers/MassEnergizeSuperAdmin/Events/CreateNewEventForm.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import states from "dan-api/data/states";
 import { withStyles } from "@material-ui/core/styles";
 import { apiCall } from "../../../utils/messenger";
+import moment from "moment";
 import MassEnergizeForm from "../_FormGenerator";
 import { getRandomStringKey } from "../ME  Tools/media library/shared/utils/utils";
 import { makeTagSection } from "./EditEventForm";
@@ -76,7 +77,11 @@ class CreateNewEventForm extends Component {
     if (!formJson) return <Loading />;
     return (
       <div key={this.state.reRenderKey}>
-        <MassEnergizeForm classes={classes} formJson={formJson} />
+        <MassEnergizeForm
+          classes={classes}
+          formJson={formJson}
+          validator={validator}
+        />
       </div>
     );
   }
@@ -98,6 +103,26 @@ const CreateEventMapped = connect(mapStateToProps)(CreateNewEventForm);
 
 export default withStyles(styles, { withTheme: true })(CreateEventMapped);
 
+const validator = (cleaned) => {
+  const start = (cleaned || {})["start_date_and_time"];
+  const end = (cleaned || {})["end_date_and_time"];
+  const endDateComesLater = new Date(end) > new Date(start);
+  return [
+    endDateComesLater,
+    !endDateComesLater &&
+      "Please provide an end date that comes later than your start date",
+  ];
+};
+const prefillEndDate = (formData, field) => {
+  formData = formData || {};
+  const start = formData["start_date_and_time"] || "";
+  const end = formData["end_date_and_time"] || "";
+  const endValueIsStillDefault =
+    end.toString() === field.defaultValue.toString();
+
+  if (endValueIsStillDefault) return start;
+  return end;
+};
 const createFormJson = ({ communities, auth }) => {
   // const { communities } = this.state;
   const is_super_admin = auth && auth.is_super_admin;
@@ -154,6 +179,7 @@ const createFormJson = ({ communities, auth }) => {
             isRequired: true,
             defaultValue: "",
             dbName: "start_date_and_time",
+            minDate: moment.utc(moment.now()),
             readOnly: false,
           },
           {
@@ -163,8 +189,10 @@ const createFormJson = ({ communities, auth }) => {
             fieldType: "DateTime",
             contentType: "text",
             isRequired: true,
-            defaultValue: "",
+            valueExtractor: prefillEndDate,
+            defaultValue: moment.utc(moment.now()),
             dbName: "end_date_and_time",
+            minDate: moment.utc(moment.now()),
             readOnly: false,
           },
           {
@@ -283,13 +311,13 @@ const createFormJson = ({ communities, auth }) => {
                 },
               }
             : {
-              name: "community",
-              label: "Primary Community (select one)",
-              fieldType: "Dropdown",
-              defaultValue: communities[0].id,
-              dbName: "community_id",
-              data: [{ displayName: "--", id: "" }, ...communities],
-            },
+                name: "community",
+                label: "Primary Community (select one)",
+                fieldType: "Dropdown",
+                defaultValue: communities[0] && communities[0].id,
+                dbName: "community_id",
+                data: [{ displayName: "--", id: "" }, ...communities],
+              },
         ],
       },
       {
