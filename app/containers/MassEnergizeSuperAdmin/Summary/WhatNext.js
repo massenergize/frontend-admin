c5861fa02fb12a3bda8369e3184252958c0ca377
frimpongopoku <pongofrimi@Gmail.com>
2022-11-28 16:55:45 +0400

    ui changes for summary items block

---
+++ app/containers/MassEnergizeSuperAdmin/Summary/WhatNext.js
@@ -0,0 +1,47 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+import { PapperBlock } from "dan-components";
+function WhatNext() {
+  return (
+    <PapperBlock
+      whiteBg
+      icon="ios-help"
+      title="What to do next?"
+      desc="Here are items you need to take care of"
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          width: "100%",
+        }}
+      >
+        {[1, 2, 3, 4, 5].map((_) => (
+          <div className="next-section-item">
+            <Typography variant="h6" style={{ fontSize: 16 }}>
+              MESSAGES
+            </Typography>
+            <Typography
+              className="text"
+              variant="body2"
+              style={{
+                display: "inline",
+                paddingBottom: 7,
+              }}
+            >
+              <span className="me-badge">65</span>
+              You have 65 messages that you have not answered{" "}
+              <span
+                style={{ marginLeft: 6 }}
+                className="fa fa-long-arrow-right"
+              />
+            </Typography>
+          </div>
+        ))}
+      </div>
+    </PapperBlock>
+  );
+}
+
+export default WhatNext;
