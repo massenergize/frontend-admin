c057eaf627c2386e256c82355a7bd95e065c64cf
tahiru <abdullai.tahiru@gmail.com>
2023-03-01 14:45:59 +0000

    fixed edit community

--- app/containers/MassEnergizeSuperAdmin/Summary/CommunitySwitch.js
+++ app/containers/MassEnergizeSuperAdmin/Summary/CommunitySwitch.js
@@ -34,10 +34,10 @@ class CommunitySwitch extends PureComponent {
   getDropdownCommunities() {
     var { auth, communities } = this.props;
 
-    communities = communities && communities.length || [];
+    communities = communities?.length? communities : [];
     var firstCom = communities[0] || {};
     if (!auth) return [];
-    if (auth.is_super_admin) return [communities, communities[0] || firstCom];
+    if (auth?.is_super_admin) return [communities, communities[0] || firstCom];
     communities = (auth && auth.admin_at) || [];
     var firstCom = communities[0] || {};
     if (auth.is_community_admin) return [communities, firstCom];
@@ -50,6 +50,7 @@ class CommunitySwitch extends PureComponent {
     const communityName = selected_community
       ? selected_community.name
       : (first && first.name) || "-------";
+
     return (
       <div>
         <Paper style={{ padding: 20, marginBottom: 10 }}>
@@ -75,7 +76,7 @@ class CommunitySwitch extends PureComponent {
             margin="normal"
             variant="outlined"
           >
-            {communities.map((option) => (
+            {communities?.map((option) => (
               <MenuItem key={option.id.toString()} value={option.name}>
                 {option.name}
               </MenuItem>
