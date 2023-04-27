9a54069c2da1538c0fbfa237983be4d1d524c30a
tahiru <abdullai.tahiru@gmail.com>
2023-04-27 11:01:57 +0000

    carbon equivalency creation form fix and actions delete modal message correction

--- app/containers/MassEnergizeSuperAdmin/CarbonEquivalencies/AllCarbonEquivalencies.js
+++ app/containers/MassEnergizeSuperAdmin/CarbonEquivalencies/AllCarbonEquivalencies.js
@@ -12,6 +12,11 @@ import LinearProgress from '@mui/material/LinearProgress';
 import Grid from '@mui/material/Grid';
 import { apiCall } from '../../../utils/messenger';
 import styles from '../../../components/Widget/widget-jss';
+import { reduxToggleUniversalModal, reduxToggleUniversalToast } from '../../../redux/redux-actions/adminActions';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Typography } from '@mui/material';
+
 
 class AllCarbonEquivalencies extends React.Component {
   constructor(props) {
@@ -20,19 +25,17 @@ class AllCarbonEquivalencies extends React.Component {
   }
 
   async componentDidMount() {
-    const response = await apiCall('/data.carbonEquivalency.get');
+    const response = await apiCall("/data.carbonEquivalency.get");
 
     if (response && response.success) {
-      const data = response.data.map(d => (
-        [
-          d.id,
-          `${d.name}...`.substring(0, 30), // limit to first 30 chars
-          d.value,
-          d.id,
-          //d.explanation,
-          //d.reference,
-        ]
-      ));
+      const data = response.data.map((d) => [
+        d.id,
+        `${d.name}...`.substring(0, 30), // limit to first 30 chars
+        d.value,
+        d.id,
+        //d.explanation,
+        //d.reference,
+      ]);
       await this.setStateAsync({ data, loading: false });
     }
   }
@@ -48,29 +51,29 @@ class AllCarbonEquivalencies extends React.Component {
 
     const cols = [
       {
-        name: 'id',
-        key: 'id',
-        options: {
-          filter: false
-        }
-      },
-      {
-        name: 'Name',
-        key: 'name',
+        name: "id",
+        key: "id",
         options: {
           filter: false,
-        }
+        },
       },
       {
-        name: 'Value',
-        key: 'value',
+        name: "Name",
+        key: "name",
         options: {
           filter: false,
-        }
+        },
       },
       {
-        name: 'Edit?',
-        key: 'edit_or_copy',
+        name: "Value",
+        key: "value",
+        options: {
+          filter: false,
+        },
+      },
+      {
+        name: "Edit?",
+        key: "edit_or_copy",
         options: {
           filter: false,
           download: false,
@@ -79,39 +82,85 @@ class AllCarbonEquivalencies extends React.Component {
               <Link to={`/admin/edit/${id}/carbon-equivalency`} target="_blank">
                 <EditIcon size="small" variant="outlined" color="secondary" />
               </Link>
-            &nbsp;&nbsp;
+              &nbsp;&nbsp;
             </div>
-          )
-        }
+          ),
+        },
       },
     ];
 
     return cols;
+  };
+
+  makeDeleteUI({ idsToDelete }) {
+    const len = (idsToDelete && idsToDelete.length) || 0;
+    return (
+      <Typography>
+        Are you sure you want to delete (
+        {(idsToDelete && idsToDelete.length) || ""})
+        {len === 1 ? " carbon equivalent ? " : " carbon equivalence? "}
+      </Typography>
+    );
+  }
+  nowDelete({ idsToDelete }) {
+    let {data} = this.state
+    const ids = [];
+    idsToDelete.forEach((d) => {
+      const found = data[d.dataIndex][0];
+      ids.push(found);
+       apiCall("/data.carbonEquivalency.delete", { id: found }).then((response) => {
+        if (response.success) {
+              const rem = (data || []).filter((item) => item[0]?.toString() !== found?.toString());
+              this.setState({data:rem})
+          this.props.toggleToast({
+            open: true,
+            message: "Carbon Equivalent data successfully deleted",
+            variant: "success",
+          });
+        } else {
+          this.props.toggleToast({
+            open: true,
+            message:
+              "An error occurred while deleting the Carbon Equivalent data",
+            variant: "error",
+          });
+        }
+      });
+    });
+
   }
 
-
   render() {
-    const title = brand.name + ' - All Carbon Equivalencies';
+    const title = brand.name + " - All Carbon Equivalencies";
     const description = brand.desc;
     const { data, columns, loading } = this.state;
     const { classes } = this.props;
     const options = {
-      filterType: 'dropdown',
-      responsive: 'stacked',
+      filterType: "dropdown",
+      responsive: "standard",
       print: true,
       rowsPerPage: 25,
       rowsPerPageOptions: [10, 25, 100],
       onRowsDelete: (rowsDeleted) => {
         const idsToDelete = rowsDeleted.data;
-        idsToDelete.forEach(d => {
-          const rowId = data[d.dataIndex][0];
-          apiCall('/data.carbonEquivalency.delete', { id: rowId });
+        this.props.toggleDeleteConfirmation({
+          show: true,
+          component: this.makeDeleteUI({ idsToDelete }),
+          onConfirm: () => this.nowDelete({ idsToDelete }),
+          closeAfterConfirmation: true,
         });
-      }
+        return false;
+      },
     };
     if (loading) {
       return (
-        <Grid container spacing={24} alignItems="flex-start" direction="row" justify="center">
+        <Grid
+          container
+          spacing={24}
+          alignItems="flex-start"
+          direction="row"
+          justify="center"
+        >
           <Grid item xs={12} md={6}>
             <Paper className={classes.root}>
               <div className={classes.root}>
@@ -126,7 +175,6 @@ class AllCarbonEquivalencies extends React.Component {
       );
     }
     return (
-
       <div>
         <Helmet>
           <title>{title}</title>
@@ -144,11 +192,24 @@ class AllCarbonEquivalencies extends React.Component {
             options={options}
           />
         </div>
-
       </div>
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      toggleDeleteConfirmation: reduxToggleUniversalModal,
+      toggleToast: reduxToggleUniversalToast,
+    },
+    dispatch
+  );
+}
+const AllCarbonEquivalenciesMapped = connect(
+  null,
+  mapDispatchToProps
+)(AllCarbonEquivalencies);
 
 // export default AllActions;
 
@@ -156,4 +217,4 @@ AllCarbonEquivalencies.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(AllCarbonEquivalencies);
+export default withStyles(styles)(AllCarbonEquivalenciesMapped);
