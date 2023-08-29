9a9610cfe657d6634488a93aecea917ec2305659
tahiru <abdullai.tahiru@gmail.com>
2023-08-29 16:15:12 +0000

    formmediaLib file reference fix

--- app/containers/MassEnergizeSuperAdmin/_FormGenerator/TinyMassEnergizeEditor.js
+++ app/containers/MassEnergizeSuperAdmin/_FormGenerator/TinyMassEnergizeEditor.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Editor as TinyEditor } from "@tinymce/tinymce-react";
-import FormMediaLibraryImplementation from "./FormMediaLibraryImplementation";
+import FormMediaLibraryImplementation from "./media library/FormMediaLibraryImplementation";
 const TINY_MCE_API_KEY = process.env.REACT_APP_TINY_MCE_KEY;
 
 function TinyMassEnergizeEditor(props) {
