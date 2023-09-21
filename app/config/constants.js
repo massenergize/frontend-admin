a827b3d0c4ea2566fd6394bad40a3aa0f1071846
Brad Hubbard-Nelson <bradhn@mindspring.com>
2023-09-21 12:06:07 -0400

    Version 4.12.13

--- app/config/constants.js
+++ app/config/constants.js
@@ -41,7 +41,6 @@ if (IS_PROD) {
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   };
 } 
-console.log(FIREBASE_CONFIG)
 
 //  ---- setting  Community Portal routes
 let PORTAL_HOST = "https://community.massenergize.org";
