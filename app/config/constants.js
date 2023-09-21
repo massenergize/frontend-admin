7a9137b8c68538be2a1962d50c6f23801045cddf
Brad Hubbard-Nelson <bradhn@mindspring.com>
2023-09-21 12:12:55 -0400

    remove console log

--- app/config/constants.js
+++ app/config/constants.js
@@ -31,7 +31,7 @@ let FIREBASE_CONFIG = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-console.log(FIREBASE_CONFIG);
+
 if (IS_PROD) {
   FIREBASE_CONFIG = {
     ...FIREBASE_CONFIG,
