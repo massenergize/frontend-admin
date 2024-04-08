0a0ed76334d1d06492479e61639a26a71ca40c5f
frimpongopoku <pongofrimi@gmail.com>
2024-04-08 16:33:22 +0000

    feat: form body for request setup to match B.E. Now ready for B.E

--- app/containers/MassEnergizeSuperAdmin/Events/notifcation-settings/ScheduleEventNotification.js
+++ app/containers/MassEnergizeSuperAdmin/Events/notifcation-settings/ScheduleEventNotification.js
@@ -45,14 +45,14 @@ const INITIAL_STATE = OPTIONS.reduce(
   {}
 );
 
+const INITIAL_REMINDER_STATE = { amount_of_time: "", unit_of_time: "days" };
 export default function ScheduleEventNotification(props) {
   const { id, close, eventObj } = props || {}; // Contains all props, and all data in the event object
   const [state, setState] = useState({});
-  const [targetCommunities, setTargetCommunities] = useState([]); // Holds the list of communities that these settings apply to
-  const [profiles, setProfiles] = useState([]); // Holds the list of settings profiles that are on the event at any point
   const [loading, setLoading] = useState(false);
-  const [activeTab, setActiveTab] = useState("nudge-settings");
   const [notification, setNotification] = useState({});
+  const [reminders, setReminders] = useState({});
+
   const dispatch = useDispatch();
   const allEvents = useSelector((state) => state.getIn(["allEvents"]));
 
@@ -81,19 +81,6 @@ export default function ScheduleEventNotification(props) {
     });
   };
 
-  const handleChange = (event) => {
-    const { name } = event.target;
-    if (name === "never" && event.target.checked) {
-      const remainder = resetOptions();
-      return setState({
-        ...remainder,
-        never: true
-      });
-    }
-    // "Never" never really gets to use this function
-    updateNotification(name, event.target.checked);
-  };
-
   const putEventInSamePosition = (event) => {
     const { index, remainder } = findItemAtIndexAndRemainder(allEvents, (ev) => ev.id === event?.id);
     const updatedEvents = [...remainder];
@@ -101,17 +88,16 @@ export default function ScheduleEventNotification(props) {
     dispatch(loadAllEvents(updatedEvents));
   };
 
-  const hasValidValues = () => {
-    if (!targetCommunities?.length) {
-      notify("Please select the communities that these settings apply to");
-      return false;
-    }
-
-    return true;
-  };
-
   const sendChangesToBackend = () => {
     // if (!hasValidValues()) return;
+
+    const form = {
+      community_ids: [props?.community?.id],
+      event_id: eventObj?.id,
+      reminders: Object.values(reminders).map((reminder) => reminder)
+    };
+
+    return console.log("sendChangesToBackend", form);
     setLoading(true);
     setNotification({});
     // const isALL = targetCommunities?.find((com) => typeof com === "string" && com?.toLowerCase() === "all");
@@ -130,7 +116,7 @@ export default function ScheduleEventNotification(props) {
         }
         const event = response?.data;
         putEventInSamePosition(event);
-        setProfiles(event?.settings?.notifications || []);
+        // setProfiles(event?.settings?.notifications || []);
         notify("Settings updated successfully!", "success");
       })
       .catch((err) => {
@@ -138,13 +124,6 @@ export default function ScheduleEventNotification(props) {
         notify(err?.toString());
         console.log("Error updating settings", err);
       });
-  };
-
-  const updateState = (newState) => {
-    setState({
-      ...state,
-      ...(newState || {})
-    });
   };
 
   const removeProfileOnBackend = (profile) => {
@@ -168,14 +147,6 @@ export default function ScheduleEventNotification(props) {
       });
   };
 
-  const removeProfile = (profile) => {
-    const newProfiles = profiles?.filter((p) => p.id !== profile.id);
-    setProfiles(newProfiles);
-    const newObj = { ...eventObj, settings: { ...(eventObj?.settings || {}), notifications: newProfiles } };
-    putEventInSamePosition(newObj);
-    removeProfileOnBackend(profile);
-  };
-
   const makeStateObject = (notification) => {
     return { ...INITIAL_STATE, ...(notification || {}) };
   };
@@ -183,11 +154,20 @@ export default function ScheduleEventNotification(props) {
   useEffect(() => {
     const { settings: settingsObject } = props || {};
     const defaultObj = (settingsObject?.notifications || [])[0];
-    setProfiles(settingsObject?.notifications || []);
+    // setProfiles(settingsObject?.notifications || []);
     const obj = makeStateObject(defaultObj?.settings);
-    setTargetCommunities(defaultObj?.communities || []);
+    // setTargetCommunities(defaultObj?.communities || []);
     setState(obj);
   }, []);
+
+  const addNewSchedule = () => {
+    const id = new Date().getTime();
+    setReminders({ ...reminders, [id]: INITIAL_REMINDER_STATE });
+  };
+  const removeReminder = (id) => {
+    const { [id]: removed, ...rest } = reminders;
+    setReminders(rest);
+  };
 
   const { message, type } = notification || {};
   const isError = type === "error";
@@ -198,10 +178,19 @@ export default function ScheduleEventNotification(props) {
         width: "auto"
       }}
     >
-      <div style={{ padding: "0px 20px", minHeight: 400, maxHeight: 550, minWidth: 530 }}>
-        <Typography variant="h6" style={{ fontWeight: "bol", color: "black", marginTop: 10 }}>
-          {smartString(eventObj?.name, 50) || "Notification Settings"}
-        </Typography>
+      <Typography variant="h6" style={{ padding: "0px 20px", fontWeight: "bold", color: "black", marginTop: 10 }}>
+        {smartString(eventObj?.name, 50) || "Notification Settings"}
+      </Typography>
+      <div
+        style={{
+          marginBottom: 50,
+          padding: "0px 20px",
+          minHeight: 400,
+          maxHeight: 550,
+          minWidth: 530,
+          overflowY: "scroll"
+        }}
+      >
         <div style={{ padding: 10, border: "solid 1px #ab47bc", margin: "10px 0px" }}>
           <Typography variant="h6" style={{ fontWeight: "bold", color: "black", fontSize: 14, color: "#ab47bc" }}>
             EXCLUDE FROM NEWSLETTER
@@ -214,16 +203,26 @@ export default function ScheduleEventNotification(props) {
           <br />
         </div>
         <div>
-          <Accordion title={smartString(`Schedule notification for ${props?.community?.name} Members`, 60)} opened>
-            <OneSchedule prefix />
-            <OneSchedule />
+          <Accordion title={smartString(`Schedule notifications for ${props?.community?.name} members`, 60)} opened>
+            {Object.entries(reminders).map(([id, reminder], index) => (
+              <OneSchedule
+                remove={(id) => removeReminder(id)}
+                updateItem={(data) => {
+                  setReminders({ ...reminders, [id]: data });
+                }}
+                key={id}
+                reminder={{ ...reminder, id }}
+                prefix={index === 0}
+              />
+            ))}
 
             <h6
+              onClick={addNewSchedule}
               className="touchable-opacity"
               style={{ marginTop: 10, fontSize: 15, color: "rgb(171, 71, 188)", textDecoration: "underline" }}
             >
               <i className="fa fa-plus" style={{ marginRight: 3 }} />
-              Add Notification
+              Add Schedule
             </h6>
           </Accordion>
         </div>
@@ -265,7 +264,8 @@ export default function ScheduleEventNotification(props) {
   );
 }
 
-const OneSchedule = ({ prefix, suffix = true }) => {
+const OneSchedule = ({ prefix, suffix = true, reminder, updateItem, remove }) => {
+  const { amount_of_time, unit_of_time } = reminder || {};
   return (
     <div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginTop: 6 }}>
       <Typography variant="p" style={{ color: "black", opacity: prefix ? 1 : 0 }}>
@@ -273,7 +273,9 @@ const OneSchedule = ({ prefix, suffix = true }) => {
       </Typography>
 
       <input
+        onChange={(e) => updateItem({ ...reminder, amount_of_time: e.target.value })}
         placeholder="00"
+        value={amount_of_time}
         style={{
           padding: 3,
           border: "solid 1px rgb(171, 71, 188)",
@@ -286,7 +288,8 @@ const OneSchedule = ({ prefix, suffix = true }) => {
         }}
       />
       <MEDropdown
-        value={["days"]}
+        onItemSelected={(item) => updateItem({ ...reminder, unit_of_time: (item || [])[0] })}
+        value={[unit_of_time || "days"]}
         labelExtractor={(item) => item.name}
         valueExtractor={(item) => item.id}
         data={UNIT_LIST}
@@ -306,6 +309,7 @@ const OneSchedule = ({ prefix, suffix = true }) => {
       )}
 
       <i
+        onClick={() => remove(reminder?.id)}
         className=" fa fa-times"
         style={{ fontSize: 24, marginLeft: "auto", marginRight: 6, color: "#c54c4c", cursor: "pointer" }}
       />
