fb8778a7cf8ab35d7dacfc11edfe2887c3ec346a
frimpongopoku <pongofrimi@gmail.com>
2024-05-30 12:16:11 +0000

    feat: added RenderCCActionSelector

---
+++ app/containers/MassEnergizeSuperAdmin/Actions/RenderCCActionSelector.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RenderCCActionSelector() {
+  return (
+    <div>RenderCCActionSelector</div>
+  )
+}
+
+export default RenderCCActionSelector
--- app/containers/MassEnergizeSuperAdmin/_FormGenerator/Modal.js
+++ app/containers/MassEnergizeSuperAdmin/_FormGenerator/Modal.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export default function AlertDialog(props) {
   const [open, setOpen] = React.useState(false);
@@ -17,27 +17,27 @@ export default function AlertDialog(props) {
     setOpen(false);
   }
 
-  const { title, text } = props;
-  const termsAndConditions =  'These Website Standard Terms And Conditions (these “Terms” or these “Website Standard Terms And Conditions”) contained herein on this webpage, shall govern your use of this website, including all pages within this website (collectively referred to herein below as this “Website”). These Terms apply in full force and effect to your use of this Website and by using this Website, you expressly accept all terms and conditions contained herein in full. You must not use this Website, if you have any objection to any of these Website Standard Terms And Conditions.  This Website is not for use by any minors (defined as those who are not at least 18 years of age), and you must not use this Website if you a minor.'
+  const { title, text, renderTrigger } = props;
+  const termsAndConditions =
+    "These Website Standard Terms And Conditions (these “Terms” or these “Website Standard Terms And Conditions”) contained herein on this webpage, shall govern your use of this website, including all pages within this website (collectively referred to herein below as this “Website”). These Terms apply in full force and effect to your use of this Website and by using this Website, you expressly accept all terms and conditions contained herein in full. You must not use this Website, if you have any objection to any of these Website Standard Terms And Conditions.  This Website is not for use by any minors (defined as those who are not at least 18 years of age), and you must not use this Website if you a minor.";
   return (
-
     <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        {title || 'Read Terms and Conditions'}
-      </Button>
+      {renderTrigger ? (
+        renderTrigger({...props, openModal:handleClickOpen, closeModal: handleClose})
+      ) : (
+        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+          {title || "Read Terms and Conditions"}
+        </Button>
+      )}
       <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">
-          {title || 'Read Terms and Conditions'}
-        </DialogTitle>
+        <DialogTitle id="alert-dialog-title">{title || "Read Terms and Conditions"}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            { text || termsAndConditions }
-          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">{text || termsAndConditions}</DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
