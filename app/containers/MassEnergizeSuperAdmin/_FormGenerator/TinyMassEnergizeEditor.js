3769db79f5c652667ef56cd0619dd27878d044c3
frimpongopoku <pongofrimi@gmail.com>
2024-12-09 11:22:20 +0000

    google sans is not default

--- app/containers/MassEnergizeSuperAdmin/_FormGenerator/TinyMassEnergizeEditor.js
+++ app/containers/MassEnergizeSuperAdmin/_FormGenerator/TinyMassEnergizeEditor.js
@@ -3,6 +3,24 @@ import { Editor as TinyEditor } from "@tinymce/tinymce-react";
 import FormMediaLibraryImplementation from "./media library/FormMediaLibraryImplementation";
 const TINY_MCE_API_KEY = process.env.REACT_APP_TINY_MCE_KEY;
 
+const DEFAULT_FONT_FORMATS = ` Google Sans='Google Sans', sans-serif;
+    Andale Mono=andale mono,times;
+    Arial=arial,helvetica,sans-serif;
+    Arial Black=arial black,avant garde;
+    Book Antiqua=book antiqua,palatino;
+    Comic Sans MS=comic sans ms,sans-serif;
+    Courier New=courier new,courier;
+    Georgia=georgia,palatino;
+    Helvetica=helvetica;
+    Impact=impact,chicago;
+    Symbol=symbol;
+    Tahoma=tahoma,arial,helvetica,sans-serif;
+    Terminal=terminal,monaco;
+    Times New Roman=times new roman,times;
+    Trebuchet MS=trebuchet ms,geneva;
+    Verdana=verdana,geneva;
+    Webdings=webdings;
+    Wingdings=wingdings,zapf dingbats;  `;
 function TinyMassEnergizeEditor(props) {
   const [open, setOpen] = useState(false);
   const [image, setImage] = useState(null);
@@ -45,7 +63,6 @@ function TinyMassEnergizeEditor(props) {
           let ed = editor?.target?.editorCommands || {};
           setEditor(ed?.editor);
         }}
-        
         {...props}
         onEditorChange={handleEditorChange}
         toolbar="undo redo | blocks | formatselect | media_library | bold italic backcolor forecolor | alignleft aligncenter alignright alignjustify | link | bullist numlist outdent indent | fontfamily | fontsize |"
@@ -57,6 +74,16 @@ function TinyMassEnergizeEditor(props) {
           force_br_newlines: true,
           force_p_newlines: false,
           forced_root_block: "", // Needed for 3.x
+          font_formats: "Google Sans='Google Sans';",
+          font_default: "Google Sans",
+          font_family_formats: DEFAULT_FONT_FORMATS,
+          content_style: `
+            @import url('https://fonts.googleapis.com/css?family=Google+Sans:400,400i,500,500i,600,600i,700,700i,900,900i&display=swap');
+            body { 
+              font-family: Google Sans, sans-serif; 
+              font-size: 14px; 
+            }
+          `,
           ...config,
           placeholder: "Enter text here ...."
         }}
