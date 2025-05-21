41b3ac102622e8a287bc66b354aef62f96e3d570
Tahiru <42780120+abdullai-t@users.noreply.github.com>
2025-05-21 04:38:06 -0600

    fixed testimonial listing issue

--- app/containers/MassEnergizeSuperAdmin/ME  Tools/table /CustomOptions.js
+++ app/containers/MassEnergizeSuperAdmin/ME  Tools/table /CustomOptions.js
@@ -11,6 +11,7 @@ export default function CustomOptions({ data , label, endpoint, customBodyRender
      customFilterListOptions: customFilterListOptions,
      filterOptions: {
        logic: (location, filters, row) => {
+        if (filters?.length === 0) return false;
 
         if(typeof(location)=== "string"){
           if (filters.length) return !filters?.includes(location);
